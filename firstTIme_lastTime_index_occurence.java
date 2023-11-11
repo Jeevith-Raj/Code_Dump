a72cf03eca31b573b3d9323d80b27236be52c357
Jeevith-Raj <20uad028@kamarajengg.edu.in>
2023-11-11 19:27:01 +0530

    this is commit

---
+++ firstTIme_lastTime_index_occurence.java
@@ -0,0 +1,14 @@
+public class firstTIme_lastTime_index_occurence {
+    public static void main(String[] args) {
+        int n = 6;
+        int[] arr = {6,5,4,3,1,2};
+        int key = 4;
+        for(int i =0;i<arr.length;i++)
+        {
+            if(arr[i] == key)
+            {
+
+            }
+        }
+    }
+}
--- swap_kth_element.java
+++ swap_kth_element.java
@@ -1,2 +1,16 @@
 public class swap_kth_element {
+    public static void main(String[] args) {
+        int n = 8;
+        int k = 3;
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int temp = 0;
+        temp = arr[k-1];
+        arr[k-1] = arr[n-k];
+        arr[n-k] = temp;
+
+        for(int i =0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+    }
 }
